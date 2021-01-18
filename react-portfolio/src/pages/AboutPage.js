13242cb8448b4982ed7392f830b0e117c34d6263
Mathew <mathewharvey@gmail.com>
2021-01-18 17:24:54 +0800

    Added about me

--- react-portfolio/src/pages/AboutPage.js
+++ react-portfolio/src/pages/AboutPage.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Content from '../components/content'
 
 function AboutPage(props) {
     return(
-        <p>About page Works</p>
+        <div>
+            <Hero title={props.title} />
+
+            <Content>
+            <p>I like to create. I am trained as a Scientist and love to learn. I have created things, movements and experiences.</p>
+            <p> Studying biology and building businesses has taught me to build systems, to solve problems and present appropriately.</p>
+
+            <p>Whether I’m building computer systems, software systems or human systems the key principles have remained constant. Develop sound foundations that are fit for purpose and allow organic growth and further development.</p>
+
+            <p>As a graduate web developer I want to bring this experience to your organisation.</p>
+
+            <p>Def. ‘Nerd’: A nerd is someone who thinks the purpose of communication is submitting one's ideas to peer review. - Joscha Bach 2018</p>
+            </Content>
+        </div>
     )
     
 }
