af413182efb8b68b9664b720cd610de3f051e6a7
Mathew <mathewharvey@gmail.com>
2021-01-19 14:31:04 +0800

    added an email sending (SendGrid) server.

--- react-portfolio/src/pages/ContactPage.js
+++ react-portfolio/src/pages/ContactPage.js
@@ -3,17 +3,18 @@ import Hero from '../components/Hero';
 import Content from '../components/content';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Axios from 'axios';
 
 class ContactPage extends React.Component {
-    
+
     constructor(props) {
         super(props);
         this.state = {
-            name:'',
-            email:'',
-            message:'',
+            name: '',
+            email: '',
+            message: '',
             disabled: false,
-            emailSent:'null',
+            emailSent: 'null',
         }
     }
 
@@ -23,38 +24,59 @@ class ContactPage extends React.Component {
         const name = target.name;
 
         this.setState({
-            [name]:value
+            [name]: value
         })
-     }
+    }
 
-    handleSubmit =(event) => {
+    handleSubmit = (event) => {
         event.preventDefault();
         this.setState({
-            disabled:true,
-            emailSent: false
+            disabled: true,
         });
+
+        Axios.post('http://localhost:3030/api/email', this.state)
+            .then(res => {
+                if (res.data.success) {
+                    this.setState({
+                        disabled: false,
+                        emailSent: true
+                    });
+                } else {
+                    this.setState({
+                        disabled: false,
+                        emailSent: false
+                    });
+                }
+
+            })
+            .catch(err => {
+                this.setState({
+                    disabled: false,
+                    emailSent: false
+                });
+            })
     }
 
     render() {
-        return(
+        return (
             <div>
                 <Hero title={this.props.title} />
 
                 <Content>
                     <Form onSubmit={this.handleSubmit}>
                         <Form.Group>
-                        <Form.Label htmlFor="full-name">Full Name</Form.Label>
-                        <Form.Control id='full-name' name='name' type='text' value={this.state.name} onChange={this.handleChange} />
+                            <Form.Label htmlFor="full-name">Full Name</Form.Label>
+                            <Form.Control id='full-name' name='name' type='text' value={this.state.name} onChange={this.handleChange} />
                         </Form.Group>
 
                         <Form.Group>
-                        <Form.Label htmlFor="email">Email</Form.Label>
-                        <Form.Control id='email' name='email' type='email' value={this.state.email} onChange={this.handleChange} />
+                            <Form.Label htmlFor="email">Email</Form.Label>
+                            <Form.Control id='email' name='email' type='email' value={this.state.email} onChange={this.handleChange} />
                         </Form.Group>
 
                         <Form.Group>
-                        <Form.Label htmlFor="message">Message</Form.Label>
-                        <Form.Control id='message' name='message' as='textarea' rows="3" value={this.state.message} onChange={this.handleChange} />
+                            <Form.Label htmlFor="message">Message</Form.Label>
+                            <Form.Control id='message' name='message' as='textarea' rows="3" value={this.state.message} onChange={this.handleChange} />
                         </Form.Group>
 
                         <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled}>
