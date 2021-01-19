59b3e98091087be5ffc82964037a690a79470f75
Mathew <mathewharvey@gmail.com>
2021-01-19 13:12:34 +0800

    added contact me page

--- react-portfolio/src/pages/AboutPage.js
+++ react-portfolio/src/pages/AboutPage.js
@@ -8,7 +8,9 @@ function AboutPage(props) {
             <Hero title={props.title} />
 
             <Content>
-            <p>I like to create. I am trained as a Scientist and love to learn. I have created things, movements and experiences.</p>
+            <p>Hello, my name is Mat Harvey. I'm a full stack engineer with experience in AngularJS, ExpressJS, NodeJS, mySQL, MongoDB, Html, CSS, JavaScript and React.</p>
+                
+            <p> I like to create. I am trained as a Scientist and love to learn. I have created things, movements and experiences.</p>
             <p> Studying biology and building businesses has taught me to build systems, to solve problems and present appropriately.</p>
 
             <p>Whether I’m building computer systems, software systems or human systems the key principles have remained constant. Develop sound foundations that are fit for purpose and allow organic growth and further development.</p>
